568ea097f2101a452b7ce2e7529970aa5bccaf65
Koutsouridis Loukas <phalanx.softdev@gmail.com>
2025-10-04 19:45:14 +0200

    initial signal implementation in a component

---
+++ src/app/components/counter-component/counter-component.ts
@@ -0,0 +1,24 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter-component.html',
+  styleUrl: './counter-component.css'
+})
+export class CounterComponent {
+  counter = signal(0);
+
+  increment(){
+    this.counter.update(v => v+1);
+  }
+
+  decrement(){
+    this.counter.update(v => v-1);
+  }
+
+  delete(){
+    this.counter.set(0);
+  }
+
+}
