1ec872b9905bc23b4235685b85100c68c304c095
Koutsouridis Loukas <phalanx.softdev@gmail.com>
2025-10-05 15:11:47 +0200

    signals implementation

--- src/app/components/counter-component/counter-component.ts
+++ src/app/components/counter-component/counter-component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed, effect } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-counter',
@@ -8,9 +9,22 @@ import { Component, signal } from '@angular/core';
 })
 export class CounterComponent {
   counter = signal(0);
+  counter2 = signal(2);
+  total = computed(() => this.counter()*this.counter2());
+  msg = signal('Hello');
+  /* countyList = toSignal(
+    this.http.get('/api/countries'),
+    {initialValue: []}
+  ); */
+
+  constructor() {
+    effect(()=> console.log('Message changed:',this.msg()));
+  }
+  
 
   increment(){
     this.counter.update(v => v+1);
+    this.msg.set('Hello, Angular');
   }
 
   decrement(){
