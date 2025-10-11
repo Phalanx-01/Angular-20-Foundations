a0deeb13a036f47b846701b31619fcbc385e1a1c
Koutsouridis Loukas <phalanx.softdev@gmail.com>
2025-10-11 22:49:03 +0200

    websocket test implementation

--- src/app/components/live-search-component/live-search-component.ts
+++ src/app/components/live-search-component/live-search-component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { debounceTime, distinctUntilChanged, fromEvent, map, Observable, switchMap } from 'rxjs';
+import { webSocket } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-live-search',
@@ -12,6 +13,9 @@ import { debounceTime, distinctUntilChanged, fromEvent, map, Observable, switchM
 export class LiveSearchComponent implements AfterViewInit{
 @ViewChild('searchBox') searchBox!:ElementRef;
 users$!:Observable<any[]>;
+socket$ = webSocket({
+  url: `wss://echo.websocket.org`,
+deserializer: (msg) => msg.data});
 
 constructor(private http: HttpClient) {}
 
@@ -24,5 +28,12 @@ ngAfterViewInit(){
       this.http.get<any[]>(`https://jsonplaceholder.typicode.com/users?q=${query}`)
     )
   );
+
+  this.socket$.subscribe(
+  msg => console.log('Received', msg), 
+  err => console.error(err),
+  ()=>console.log('Complete')
+  );
  }
+
 }
