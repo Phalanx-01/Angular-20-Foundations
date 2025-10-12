ed4bfa3716f1e2b7db62bd57b4ee4036d21ef801
Koutsouridis Loukas <phalanx.softdev@gmail.com>
2025-10-12 22:25:53 +0200

    dynamic form array added. handling list of inputs

--- src/app/components/register-form/register-form.ts
+++ src/app/components/register-form/register-form.ts
@@ -1,11 +1,12 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 
 interface RegistrationData{
   username: string;
   email: string;
   password: string;
+  phones: string[];
 }
 
 function passwordStrengthValidator(control: AbstractControl): ValidationErrors | null {
@@ -23,7 +24,8 @@ export class RegisterForm {
  registerForm: FormGroup<{
   username: FormControl<string>,
   email: FormControl<string>,
-  password: FormControl<string>
+  password: FormControl<string>,
+  phones: FormArray<FormControl<string>>
  }>;
 
  constructor(private fb: FormBuilder){
@@ -31,14 +33,33 @@ export class RegisterForm {
     username: this.fb.control('',{ validators:[Validators.required], nonNullable:true }),
     email: this.fb.control('',{validators:[Validators.required, Validators.email], nonNullable:true }),
     password: this.fb.control('',{ validators: [Validators.required, Validators.minLength(6), passwordStrengthValidator], nonNullable: true }),
+    phones: this.fb.array<FormControl<string>>([])
   });
  }
+
+ get phones() {
+  return this.registerForm.get('phones') as FormArray<FormControl<string>>;
+ }
+
+ addPhone() {
+  const control = this.fb.control('', { nonNullable:true });
+  this.phones.push(control);
+ }
+
+removePhone(index: number) {
+  this.phones.removeAt(index);
+}
 
 submit(){
   if(this.registerForm.valid){
     const data: RegistrationData = this.registerForm.getRawValue();
+    const cleanPhones = data.phones.filter(p => p && p.trim() !== '');
+    const phoneData: RegistrationData = {
+      ...data,
+      phones: cleanPhones
+    };
     alert('Registered: ' + JSON.stringify(data));
   }
-}
+ }
 
 }
